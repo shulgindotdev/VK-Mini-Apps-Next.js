dd28cc30c12136246c2ae05add02b4ddfd3c8649
Dmitrii Shulgin <dmshulgin0@gmail.com>
2020-04-09 10:15:30 +0700

    add link to About page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const About = () => (
+  <div className="container">
+    <h1>About us (About)</h1>
+  </div>
+)
+
+export default About
+
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import bridge from '@vkontakte/vk-bridge';
 
 bridge.send('VKWebAppInit');
@@ -5,7 +6,10 @@ bridge.subscribe(e => console.log(e));
 
 const Index = () => (
   <div className="container">
-    <h1>Hello, World!</h1>
+    <h1>Hello, World! (Index)</h1>
+    <Link href="/about">
+      <a>About Us</a>
+    </Link>
   </div>
 )
 
