b905543f1ecc0175cda9bdb0b33354ffe84ac191
Dmitrii Shulgin <dmshulgin0@gmail.com>
2020-04-09 10:38:15 +0700

    fix vk bridge init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import bridge from '@vkontakte/vk-bridge';
 
-bridge.send('VKWebAppInit');
+bridge.send("VKWebAppInit", {});
 bridge.subscribe(e => console.log(e));
 
 const Index = () => (
