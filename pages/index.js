8922f766bd5ea9599142bb4f171b54a2cba3c1e6
Dmitrii Shulgin <dmshulgin0@gmail.com>
2020-04-13 10:31:28 +0700

    remove eruda

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 import Link from 'next/link'
 import bridge from '@vkontakte/vk-bridge';
-import eruda from 'eruda/eruda';
-
-eruda.init();
-
-const console = eruda.get('console');
-console.config.set('catchGlobalErr', true);
 
 bridge.send("VKWebAppInit", {});
 bridge.subscribe(e => console.log(e));
