3fb33eb010c6cc95f8148f7eef4a00dbdf62ed78
Dmitrii Shulgin <dmshulgin0@gmail.com>
2020-04-13 10:07:09 +0700

    add eruda

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Link from 'next/link'
 import bridge from '@vkontakte/vk-bridge';
+import eruda from 'eruda';
+
+const console = eruda.get('console');
+console.config.set('catchGlobalErr', true);
 
 bridge.send("VKWebAppInit", {});
 bridge.subscribe(e => console.log(e));
